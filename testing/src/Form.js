e68505c4c891c6f56a138c17d85c16718bf08d77
Elif Seçkin <74016654+secginelif@users.noreply.github.com>
2023-01-30 12:24:21 +0300

    unit_test

---
+++ testing/src/Form.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <Form>
+            
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default Form;
